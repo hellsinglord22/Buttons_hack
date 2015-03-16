012e1e5f5f9233612a43ee53de39b059cb40b64b
H <heshamscorpe55@gmail.com>
2015-03-16 16:20:04 +0200

    Add form and place it on the frame(Windows)

--- src/Form.java
+++ src/Form.java
@@ -6,6 +6,31 @@ import java.awt.*;
  * @Date: 3/16/2015.
  */
 public class Form extends JPanel {
+    /*Attributes*/
+    private GridBagConstraints formConstrain;
+
+    /*Constructor*/
+    public Form() {
+        /// Initialize Attributes ///
+        formConstrain = new GridBagConstraints();
 
 
+        // properties //
+        setLayout(new GridBagLayout());
+
+        // position attributes //
+
+        // PREPOSITIONING PROPERTIES //
+        formConstrain.fill = GridBagConstraints.NONE;
+
+    }
+
+    /*Methods*/
+    // Method to help position elements on the form //
+    private void setCurrentPosition(int row, int column) {
+        formConstrain.gridx = column;
+        formConstrain.gridy = row;
+    }
 }
+
+
