5db732a1ff7e90fe53048eeef87fdb797f410f16
H <heshamscorpe55@gmail.com>
2015-03-16 16:27:15 +0200

    Add and position name label and field into the form

--- src/Form.java
+++ src/Form.java
@@ -1,3 +1,5 @@
+import sun.text.resources.FormatData_zh_CN;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,12 +10,15 @@ import java.awt.*;
 public class Form extends JPanel {
     /*Attributes*/
     private GridBagConstraints formConstrain;
+    private JLabel nameLabel;
+    private JTextField nameTextField;
 
     /*Constructor*/
     public Form() {
         /// Initialize Attributes ///
         formConstrain = new GridBagConstraints();
-
+        nameLabel = new JLabel("Name: ");
+        nameTextField = new JTextField(15);
 
         // properties //
         setLayout(new GridBagLayout());
@@ -22,6 +27,19 @@ public class Form extends JPanel {
 
         // PREPOSITIONING PROPERTIES //
         formConstrain.fill = GridBagConstraints.NONE;
+
+        // first row //
+        formConstrain.weightx = 0.1 ;
+        formConstrain.weighty = 0.1;
+
+        formConstrain.anchor = GridBagConstraints.LINE_END;
+        setCurrentPosition(0, 0);
+        add(nameLabel, formConstrain);
+
+        formConstrain.anchor = GridBagConstraints.LINE_START;
+        setCurrentPosition(0, 1);
+        add(nameTextField, formConstrain);
+
 
     }
 
