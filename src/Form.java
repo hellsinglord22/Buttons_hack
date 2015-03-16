69138f1e34b4e0540ee84a8ae90fdcdbdf4d40ce
H <heshamscorpe55@gmail.com>
2015-03-16 17:01:24 +0200

    Add age label and list to the form

--- src/Form.java
+++ src/Form.java
@@ -12,18 +12,29 @@ import java.awt.*;
 public class Form extends JPanel {
     /*Attributes*/
     private GridBagConstraints formConstrain;
-    private JLabel nameLabel;
+    private JLabel nameLabel,ageLabel;
     private JTextField nameTextField;
     private Border innerBorder , outerBorder;
-
+    private JList ageList;
+    private DefaultListModel ageModel;
     /*Constructor*/
     public Form() {
         /// Initialize Attributes ///
         formConstrain = new GridBagConstraints();
         nameLabel = new JLabel("Name: ");
+        ageLabel = new JLabel("Age: ");
         nameTextField = new JTextField(15);
         innerBorder = BorderFactory.createTitledBorder("hacking form");
         outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        ageList = new JList();
+        ageModel = new DefaultListModel();
+
+        // add other attributes properties //
+        ageModel.addElement("under 18");
+        ageModel.addElement("18 to 65");
+        ageModel.addElement("Over 65");
+        ageList.setModel(ageModel);
+
 
         // properties //
         setLayout(new GridBagLayout());
@@ -33,11 +44,10 @@ public class Form extends JPanel {
 
         // PREPOSITIONING PROPERTIES //
         formConstrain.fill = GridBagConstraints.NONE;
-
-        // first row //
         formConstrain.weightx = 0.1 ;
         formConstrain.weighty = 0.1;
 
+        // first row //
         formConstrain.anchor = GridBagConstraints.LINE_END;
         setCurrentPosition(0, 0);
         add(nameLabel, formConstrain);
@@ -45,6 +55,15 @@ public class Form extends JPanel {
         formConstrain.anchor = GridBagConstraints.LINE_START;
         setCurrentPosition(0, 1);
         add(nameTextField, formConstrain);
+
+        // second row //
+        formConstrain.anchor = GridBagConstraints.LINE_END;
+        setCurrentPosition(1, 0);
+        add(ageLabel, formConstrain);
+
+        formConstrain.anchor = GridBagConstraints.LINE_START;
+        setCurrentPosition(1, 1);
+        add(ageList, formConstrain);
 
 
     }
