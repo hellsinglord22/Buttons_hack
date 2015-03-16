aba097c52a3b68d8946d1b89c922f9f8339c8d98
H <heshamscorpe55@gmail.com>
2015-03-16 16:04:59 +0200

    Create form blue print and add it to the MainFrame

---
+++ src/Form.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @Author: Scorpion
+ * @Date: 3/16/2015.
+ */
+public class Form extends JPanel {
+
+
+}
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,10 +5,11 @@ import java.awt.*;
  * @Author: Scorpion
  * @Date: 3/16/2015.
  */
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame {
 
     /*Attributes*/
-
+    private Form mainFrameForm;
+    private Container mainFrameContainer;
 
 
     /* Constructor*/
@@ -17,12 +18,19 @@ public class MainFrame extends JFrame{
         super(title);
 
         /// Initialize attributes //
+        mainFrameForm = new Form();
+        mainFrameContainer = getContentPane();
+
 
         /// set properties //
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLayout(new BorderLayout());
+
+        // position attributes on the frame (windows) //
+        mainFrameContainer.add(mainFrameForm, BorderLayout.CENTER);
+
 
     }
 
