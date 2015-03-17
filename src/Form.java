2d816f615a305fd02a225cf98c7232d946789a2c
H <heshamscorpe55@gmail.com>
2015-03-18 01:57:30 +0200

    Add a ComboBox 'sexComboBox'  to the Form

--- src/Form.java
+++ src/Form.java
@@ -1,3 +1,4 @@
+import javafx.scene.control.ComboBox;
 import sun.text.resources.FormatData_zh_CN;
 
 import javax.swing.*;
@@ -14,12 +15,14 @@ import java.awt.event.ActionListener;
 public class Form extends JPanel {
     /*Attributes*/
     private GridBagConstraints formConstrain;
-    private JLabel nameLabel,ageLabel;
+    private JLabel nameLabel,ageLabel, sexLabel;
     private JTextField nameTextField;
     private Border innerBorder , outerBorder;
     private JList ageList;
     private DefaultListModel ageModel;
     private JButton okButton;
+    private JComboBox sexComboBox;
+    private DefaultComboBoxModel sexModel;
 
     /*Constructor*/
     public Form() {
@@ -27,14 +30,18 @@ public class Form extends JPanel {
         formConstrain = new GridBagConstraints();
         nameLabel = new JLabel("Name: ");
         ageLabel = new JLabel("Age: ");
+        sexLabel = new JLabel("Sex: ");
         nameTextField = new JTextField(15);
         okButton = new JButton("OK");
         innerBorder = BorderFactory.createTitledBorder("hacking form");
         outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
         ageList = new JList();
+        sexComboBox = new JComboBox();
         ageModel = new DefaultListModel();
+        sexModel = new DefaultComboBoxModel();
 
         // add other attributes properties //
+        /// list properties //
         ageModel.addElement(new AgeCategory(0, "under 18"));
         ageModel.addElement(new AgeCategory(1, "18 to 65"));
         ageModel.addElement(new AgeCategory(2, "Over 65"));
@@ -43,6 +50,12 @@ public class Form extends JPanel {
         ageList.setSelectedIndex(1); /// select the element in the middle
         ageList.setSize(110, 70);
         ageList.setBorder(BorderFactory.createEtchedBorder());
+
+        // ComboBox properties ///
+        sexModel.addElement("male");
+        sexModel.addElement("female");
+        sexModel.addElement("others");
+        sexComboBox.setModel(sexModel);
 
 
         // properties //
@@ -56,7 +69,9 @@ public class Form extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 AgeCategory age = (AgeCategory) ageList.getSelectedValue();
+                String sex = (String) sexComboBox.getSelectedItem();
                 System.out.println("Age: " + age + "\n");
+                System.out.println("Sex: " + sex + "\n");
             }
         });
 
@@ -89,12 +104,20 @@ public class Form extends JPanel {
         setCurrentPosition(1, 1);
         add(ageList, formConstrain);
 
-        // third row ///
+        // 3rd row ///
+        formConstrain.anchor = GridBagConstraints.LINE_END;
+        setCurrentPosition(2, 0);
+        add(sexLabel, formConstrain);
 
+        formConstrain.anchor = GridBagConstraints.LINE_START;
+        setCurrentPosition(2, 1);
+        add(sexComboBox, formConstrain);
+
+        // fourth row ///
         // lets change the weight of the row to make it take more space and push other elements to the top
         formConstrain.weighty = 2;
         formConstrain.anchor = GridBagConstraints.LINE_END;
-        setCurrentPosition(2, 0);
+        setCurrentPosition(3, 0);
         add(okButton, formConstrain);
 
 
